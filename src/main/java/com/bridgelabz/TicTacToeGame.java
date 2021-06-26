816de31bfbec0a0b4f3af9a0cffa4c61df867cef
shwetap29 <shwetaprasad48@gmail.com>
2021-06-27 00:17:02 +0530

    ADDED:Ability To Select A Choice

--- src/main/java/com/bridgelabz/TicTacToeGame.java
+++ src/main/java/com/bridgelabz/TicTacToeGame.java
@@ -1,12 +1,14 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
     //Constants
     private static final int USER=1;
     private static final int COMPUTER=2;
     private static final int EMPTY=0;
 
-    //  method to print the board by creating multi dimentional 2D array
+    //   creating a method to print 2D board
     public static void print_board(int[][] board){
         System.out.println(printChar(board[0][0]));
         System.out.println("|");
@@ -26,8 +28,8 @@ public class TicTacToeGame {
         System.out.println("|");
         System.out.println(printChar(board[2][2]));
     }
-
-    private static char printChar(int b) {
+//method to return empty or x or o according to player
+    public static char printChar(int b) {
         switch (b){
             case EMPTY:
                 return ' ';
@@ -38,14 +40,43 @@ public class TicTacToeGame {
         }
         return  ' ';
     }
+//method to give user a choice to select X Or O
+    public static String moveChoice() {
+        String computerChoice;
+        String userChoice;
+        System.out.println("Choose 1. for X and 2. for O");
+        Scanner sc = new Scanner(System.in);
+        int userChoiceNumber = sc.nextInt();
+        if (userChoiceNumber == 1) {
+            userChoice = "X";
+        } else
+            userChoice = "O";
+        System.out.println("You have choosen: " + userChoice);
+
+        return userChoice;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome To TicTacToe Game");
-
+        System.out.println("Welcome to TicTacToe Game");
         int turn;
+        String computerChoice;
         //creating multidimentional array
         int[][] board = new int[3][3];
-        //calling method to print the board
+        //calling a method to print board
         print_board(board);
+
+        //calling method to select user choice
+        		String userChoice = moveChoice();
+        		if (userChoice == "X") {
+        			  computerChoice = "O";
+        			  turn = COMPUTER;
+        		 }
+        			 else {
+        			 	computerChoice = "X";
+        				turn = USER;
+        		}
+        		 System.out.println("You have chosen: " + userChoice + " so computer gets: " + computerChoice);
     }
-}
+
+    }
+
